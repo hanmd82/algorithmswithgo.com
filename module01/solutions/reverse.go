13e3ce454fb8d6292151ddf01450f7902ba06dea
Mingding, Han <hanmd82@gmail.com>
2020-03-28 16:46:41 +0800

    improve: Use 'strings.Builder' which is more memory efficient
    - https://www.calhoun.io/concatenating-and-building-strings-in-go

--- module01/solutions/reverse.go
+++ module01/solutions/reverse.go
@@ -1,5 +1,7 @@
 package module01solutions
 
+import "strings"
+
 // Reverse will return the provided word in reverse
 // order. Eg:
 //
@@ -7,11 +9,17 @@ package module01solutions
 //   Reverse("alphabet") => "tebahpla"
 //
 func Reverse(word string) string {
-	len := len(word)
-	result := make([]rune, len)
+	// len := len(word)
+	// result := make([]rune, len)
 
-	for i,v := range(word) {
-		result[len-i-1] = v
+	// for i,v := range(word) {
+	// 	result[len-i-1] = v
+	// }
+	// return string(result)
+
+	var sb strings.Builder
+	for i := len(word)-1; i >= 0; i-- {
+		sb.WriteByte(word[i])
 	}
-	return string(result)
+	return sb.String()
 }
