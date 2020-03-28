b68302eceb1f0435ddd640a5b487144f9b0f218e
Mingding, Han <hanmd82@gmail.com>
2020-03-28 16:24:31 +0800

    done: alternative recursive solution to Sum

--- module01/solutions/sum.go
+++ module01/solutions/sum.go
@@ -1,11 +1,18 @@
 package module01solutions
 
-// Sum will sum up all of the numbers passed
-// in and return the result
+// // Sum will sum up all of the numbers passed
+// // in and return the result
+// func Sum(numbers []int) int {
+// 	sum := 0
+// 	for _,v := range(numbers) {
+// 		sum += v
+// 	}
+// 	return sum
+// }
+
 func Sum(numbers []int) int {
-	sum := 0
-	for _,v := range(numbers) {
-		sum += v
+	if (len(numbers) == 0) {
+		return 0
 	}
-	return sum
+	return numbers[0] + Sum(numbers[1:])
 }
