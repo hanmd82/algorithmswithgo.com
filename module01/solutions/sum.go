1f4e123aa176e8c0792d43fa42fa00b6c04d8e47
Mingding, Han <hanmd82@gmail.com>
2020-03-28 16:10:08 +0800

    done: Solved Sum

---
+++ module01/solutions/sum.go
@@ -0,0 +1,11 @@
+package module01solutions
+
+// Sum will sum up all of the numbers passed
+// in and return the result
+func Sum(numbers []int) int {
+	sum := 0
+	for _,v := range(numbers) {
+		sum += v
+	}
+	return sum
+}
