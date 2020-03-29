c75ed6517229a61c9e199ac057c8d7d67ff67957
Mingding, Han <hanmd82@gmail.com>
2020-03-29 09:06:23 +0800

    done: Solved BaseToDec

---
+++ module01/solutions/base_to_dec.go
@@ -0,0 +1,47 @@
+package module01solutions
+
+import "math"
+
+// BaseToDec takes in a number and the base it is currently
+// in and returns the decimal equivalent as an integer.
+//
+// Eg:
+//
+//   BaseToDec("E", 16) => 14
+//   BaseToDec("1110", 2) => 14
+//
+func BaseToDec(value string, base int) int {
+	const charset = "0123456789ABCDEF"
+	var res int
+	value = Reverse(value)
+
+	for i,v := range(value) {
+		res += IndexOf(charset, v) * PowInt(base, i)
+	}
+	return res
+}
+
+func PowInt(x, y int) int {
+	return int(math.Pow(float64(x), float64(y)))
+}
+
+func IndexOf(word string, c rune) int {
+	for i,v := range(word) {
+		if c == v {
+			return i
+		}
+	}
+	return 0
+}
+
+// // simple example for BinaryToDec
+// func BinaryToDec(value string) int {
+// 	var res int
+// 	value = Reverse(value)
+
+// 	for i,v := range(value) {
+// 		factor := int(v-'0')
+// 		res += factor * PowInt(2, i)
+// 	}
+// 	return res
+// }
