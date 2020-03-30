f525b837a294fece30a6b05ec35f47c22ca7d70d
Mingding, Han <hanmd82@gmail.com>
2020-03-30 09:35:52 +0800

    done: Solved Factor

---
+++ module01/solutions/factor.go
@@ -0,0 +1,40 @@
+package module01solutions
+
+// Factor takes in a list of primes and a number and factors that number with
+// the provided primes.
+//
+// The returned numbers can be in any order; tests will sort them in increasing
+// order to make checking easier.
+//
+// Bonus: Any remainder (aside from 1) that can't be factored will be treated as
+// a prime in the results.
+//
+// Examples:
+//
+//   Factor([]int{2,3,5}, 30) // []int{2,3,5}
+//   Factor([]int{2,3,5}, 28) // []int{2,2,7}
+//   Factor([]int{2,3,5}, 720) // []int{2,2,2,3,3,5}
+//
+// Examples with remainders:
+//
+//   Factor([2,5], 30) // []int{2,5,3}
+//   Factor([3,5], 720) // []int{3,3,5,16}
+//   Factor([], 4) // []int{4}
+//
+func Factor(primes []int, number int) []int {
+	ans := []int{}
+	for _, p := range primes {
+		for {
+			if number%p == 0 {
+				number /= p
+				ans = append(ans, p)
+			} else {
+				break
+			}
+		}
+	}
+	if number != 1 {
+		ans = append(ans, number)
+	}
+	return ans
+}
